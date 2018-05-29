103d6a159eac8afdb6e7a2ea919eed361a8d0f77
MURUGANANTHnandhu <murugananth666@gmail.com>
2018-05-29 18:32:08 +0530

    wihileloop

---
+++ src/com/basic/AddingFirstTenNumbers.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class AddingFirstTenNumbers {
+	public static void main(String[] args) {
+		
+	int x=0,total=0;
+	 while ( x <= 100 ) {
+		total+= x;
+		++x;}
+	 
+		System.out.println(total);
+		
+
+}}
+
