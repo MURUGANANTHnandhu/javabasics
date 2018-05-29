1315d207d25df55f9f6bcc0d7329351af3d1c835
MURUGANANTHnandhu <murugananth666@gmail.com>
2018-05-29 15:35:28 +0530

    stringname

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String[] args) 
+	{
+		String firstName= "murugananth";
+		String LastName= "nandhu";
+		System.out.println(firstName+" "+LastName);
+	}
+
+}
